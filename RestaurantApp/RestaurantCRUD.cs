29ee0ee89878baf5346afe259386e6e440f0c03a
Milan992 <mitic.gsm@gmail.com>
2020-06-12 13:39:17 +0200

    class body

---
+++ RestaurantApp/RestaurantCRUD.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp
+{
+    class RestaurantCRUD
+    {
+        public void CreateOrder()
+        {
+
+        }
+
+        public void DeleteOrde()
+        {
+
+        }
+
+        public void ReadOrder()
+        {
+
+        }
+
+        public void ReadAllOrders()
+        {
+
+        }
+
+        public void UpdateOrder()
+        {
+
+        }
+    }
+}
